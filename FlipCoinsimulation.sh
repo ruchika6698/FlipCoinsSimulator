596c41a35d8e6650ffde2f4f4c314c8728a65eff
ruchika <ruchika6698@gmail.com>
2020-03-23 21:06:55 +0530

    Added:Triplet Combination store in dictionary and find Percentage

--- FlipCoinsimulation.sh
+++ FlipCoinsimulation.sh
@@ -8,31 +8,30 @@ declare -A dictionary
 #flipping coin multiple times and store the singlet,doublet & triplet combination in a dictonary
 
 function flipcoin() {
-
- for (( index=0; index<$1; index++ ))
-  do
-  	Count=0
-   for (( index1=0; index1<$2; index1++ ))
-   do
-    Number=$(( RANDOM%2 ))
-    if [[ $Number -eq 1 ]]
-    then
-       totalCount+=H
-    else
-       totalCount+=T
-    fi
-  done
-  dictionary[$Count]=$(( ${dictionary[$Count]}+1 ))
-done
-echo "Count of all combination: ${dictionary[@]}"
-echo "keys                      ${!dictionary[@]}"
+	for (( index=0; index<$1; index++ ))
+  	do
+  		Count=0
+		for (( index1=0; index1<$2; index1++ ))
+   	do
+    		Number=$(( RANDOM%2 ))
+    		if [[ $Number -eq 1 ]]
+    		then
+       		totalCount+=H
+    		else
+       		totalCount+=T
+    		fi
+		done
+  		dictionary[$Count]=$(( ${dictionary[$Count]}+1 ))
+	done
+	echo "Count of all combination: ${dictionary[@]}"
+	echo "keys                      ${!dictionary[@]}"
 }
 
 #function for percentage of singlet,doublet and triplet combination
 function percentageFlipCoin() {
 	for var in ${!dictionary[@]}
 	do
-		dictionary[$var]=( echo "scale=2;$((${dictionary[$var]}))/$coins*100" | bc )
+		dictionary[$var]=$( echo "scale=2;$((${dictionary[$var]}))/$coins*100" | bc )
 	done
 }
 
@@ -42,17 +41,22 @@ echo  "Press 1 for singlet combination or Press 2 for doublet combination Press 
 read choice
 
 case $choice in
-  1)
-  flipcoin $coins $choice
-  percentageFlipCoin
-  echo "Percentage of singlet combination: ${dictionary[@]}"
-  ;;
-  2)
-  flipcoin $coins $choice
-  percentageFlipCoin
-  echo "Percentage of doublet combination: ${dictionary[@]}"
-  ;;
-  *)
-  echo "wrong input"
-  ;;
+	1)
+		flipcoin $coins $choice
+		percentageFlipCoin
+		echo "Percentage of singlet combination: ${dictionary[@]}"
+	;;
+	2)
+		flipcoin $coins $choice
+		percentageFlipCoin
+		echo "Percentage of doublet combination: ${dictionary[@]}"
+	;;
+	3)
+		flipcoin $coins $choice
+		percentageFlipCoin
+		echo "Percentage of Triplet combination: ${dictionary[@]}"
+	;;
+	*)
+ 	 echo "wrong input"
+	;;
 esac
