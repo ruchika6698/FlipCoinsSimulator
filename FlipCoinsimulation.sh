8af63c86157d9f0fd610499d0115ae270edee195
ruchika <ruchika6698@gmail.com>
2020-03-23 20:42:09 +0530

    Added:Singlet Combination store in dictionary and find Percentage

--- FlipCoinsimulation.sh
+++ FlipCoinsimulation.sh
@@ -2,12 +2,34 @@
 
 printf "Welcome to Flip Coin Simulation \n"
 
-#flipping coin to display head or tail
+printf "How many times you to want flip a coin? \n"
+read count
 
-Number=$(( RANDOM%2 ))
-if [[ $Number -eq 1 ]]
-then
-	printf "Head \n"
-else
-	printf "Tail \n"
-fi
+declare -A result
+
+#flipping coin to display head or tail
+#flipping coin multiple times and store the singlet combination in a dictonary
+
+headCount=0
+tailCount=0
+
+for (( index=0; index<$count; index++ ))
+do
+	Number=$(( RANDOM%2 ))
+	if [[ $Number -eq 1 ]]
+	then
+		printf "Head \n"
+	else
+		printf "Tail \n"
+	fi
+done
+#Storing the result in dictionary
+result[H]=$headCount
+result[T]=$tailCount
+
+#Computing Percentage of heads and Tail
+percentageOfHeads=( echo "scale=2;$headCount/$count*100" | bc )
+percentageOfTails=( echo "scale=2;$tailCount/$count *100" | bc )
+
+printf "Percentage of heads: $percentageOfHeads \n"
+printf "Percentage of tails: $percentageOfTails \n"
