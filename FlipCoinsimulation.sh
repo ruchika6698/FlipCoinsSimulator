631ff469c0cc4ee9c3d3488fff9a3e608839493b
ruchika <ruchika6698@gmail.com>
2020-03-23 20:33:41 +0530

    Added:Display Head or Tail

--- FlipCoinsimulation.sh
+++ FlipCoinsimulation.sh
@@ -2,3 +2,12 @@
 
 printf "Welcome to Flip Coin Simulation \n"
 
+#flipping coin to display head or tail
+
+Number=$(( RANDOM%2 ))
+if [[ $Number -eq 1 ]]
+then
+	printf "Head \n"
+else
+	printf "Tail \n"
+fi
